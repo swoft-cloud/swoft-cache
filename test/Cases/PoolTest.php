58f0a036f7a060ac237c8a52277b73f29f6ec8bc
lilin <794774870@qq.com>
2018-01-25 23:55:21 +0800

    add unit

---
+++ test/Cases/PoolTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Swoft\Cache\Test\Cases;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ *
+ * @uses      PoolTest
+ * @version   2018年01月25日
+ * @author    stelin <phpcrazy@126.com>
+ * @copyright Copyright 2010-2016 swoft software
+ * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ */
+class PoolTest extends TestCase
+{
+
+}
